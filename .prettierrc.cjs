af30586d926641753409c3f7d0c37ae8aac10dcc
Adi Rizky <adirizky54@gmail.com>
2022-11-15 01:51:45 +0700

    Add components header

---
+++ .prettierrc.cjs
@@ -0,0 +1,6 @@
+{
+  astroAllowShorthand: false;
+  singleQuote: false;
+  semi: false;
+  plugins: [require('prettier-plugin-tailwindcss')];
+}
---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    "postcss-focus-visible": {
+      replaceWith: "[data-focus-visible-added]",
+    },
+  },
+};
