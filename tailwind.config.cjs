97d9188d4550753e2064899cdada0e8d836aa87c
Adi Rizky <adirizky54@gmail.com>
2023-01-19 00:00:32 +0700

    remove uneceserry config tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,6 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      borderRadius: {
-        "4xl": "2rem",
-      },
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
