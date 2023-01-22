29a366488827a3127bcde446258edad8561f37ff
Adi Rizky <adirizky54@gmail.com>
2023-01-23 00:27:11 +0700

    wip list project

--- src/components/FeaturedProject.tsx
+++ src/components/FeaturedProject.tsx
@@ -1,10 +1,90 @@
+const posts = [
+  {
+    title: 'Boost your conversion rate',
+    href: '#',
+    category: { name: 'Article', href: '#' },
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    imageUrl:
+      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    readingTime: '6 min',
+    author: {
+      name: 'Roel Aufderehar',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  {
+    title: 'How to use search engine optimization to drive sales',
+    href: '#',
+    category: { name: 'Video', href: '#' },
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
+    date: 'Mar 10, 2020',
+    datetime: '2020-03-10',
+    imageUrl:
+      'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    readingTime: '4 min',
+    author: {
+      name: 'Brenna Goyette',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+  {
+    title: 'Improve your customer experience',
+    href: '#',
+    category: { name: 'Case Study', href: '#' },
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
+    date: 'Feb 12, 2020',
+    datetime: '2020-02-12',
+    imageUrl:
+      'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    readingTime: '11 min',
+    author: {
+      name: 'Daniela Metz',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  },
+];
+
 export function FeaturedProject() {
   return (
     <section className="relative bg-gray-900 z-[2]">
-      <span className="absolute inset-x-1 -top-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0"></span>
+      <span className="absolute inset-x-1 -top-px h-[2px] bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0"></span>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12">
         <h2 className="mt-4 text-2xl font-bold text-white lg:mt-5 lg:text-3xl">Featured Projects</h2>
         <p className="mt-3 block text-gray-300 lg:text-lg max-w-md">Here are some of my favorite projects. I‘ve included details about the project.</p>
+        <div className="mt-16">
+          <ul className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3">
+            {posts.map((post) => (
+              <li className="group relative flex flex-col">
+                <div className="relative z-10 flex-shrink-0">
+                  <img className="h-56 w-full object-cover rounded-lg" src={post.imageUrl} alt="" />
+                </div>
+                <div className="flex flex-1 flex-col justify-between mt-6">
+                  <div className="flex-1">
+                    <h2 className="text-xl font-semibold text-white">
+                      <div className="absolute -inset-y-8 -inset-x-4 z-0 scale-95 bg-gray-800/70 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl"></div>
+                      <a href={post.href}>
+                        <span className="absolute -inset-y-8 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+                        <span className="relative z-10">{post.title}</span>
+                      </a>
+                    </h2>
+                    <p className="relative z-10 mt-2 text-base text-gray-400">{post.description}</p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   )
